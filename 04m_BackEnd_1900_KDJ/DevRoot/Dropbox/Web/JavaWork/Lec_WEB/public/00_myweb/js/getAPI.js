d71e97171fa4b9e8beede36fcaac6b5e5c3d13b6
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-07-30 04:42:20 +0900

    BackEnd myweb update

--- 04m_BackEnd_1900_KDJ/DevRoot/Dropbox/Web/JavaWork/Lec_WEB/public/00_myweb/js/getAPI.js
+++ 04m_BackEnd_1900_KDJ/DevRoot/Dropbox/Web/JavaWork/Lec_WEB/public/00_myweb/js/getAPI.js
@@ -1,4 +1,5 @@
 const API_KEY = "J8Trk79a5dDBsVwfEuSXyoNnLG4OHKmZp";
+let bData, aData = [];
 
 function getAPI() {
    let url, url2, xhttp;
@@ -9,7 +10,8 @@ function getAPI() {
 		if(this.readyState == 4 && this.status == 200) {
 			//데이터 받고 실행할 함수
 			let data = JSON.parse(this.responseText);
-			parseJSON1(data, document.getElementById('countrybox').value);
+			bData = data;
+			parseJSON1(document.getElementById('countrybox').value);
 			setRank1(data);
 		}
 	};
@@ -20,30 +22,31 @@ function getAPI() {
 		if(this.readyState == 4 && this.status == 200){
 			//데이터 받고 실행할 함수
 			let data = JSON.parse(this.responseText);
+			aData = data;
 			setRank2(data);
-			parseJSON2(data, JSON.parse(this.responseText));
+			parseJSON2(data);
 		}
 	}
 	xhttp.open("GET", url2, true);
 	xhttp.send();
 }
 //data1/////////////////////////////////////////////////////////////
-function parseJSON1(bData, country) {
-	document.getElementById('countryName2').innerText = bData[`${country}`]["countryName"]
-	document.getElementById('newCase2').innerText = bData[`${country}`]["newCase"]
-	document.getElementById('totalCase2').innerText = bData[`${country}`]["totalCase"]
-	document.getElementById('recovered2').innerText = bData[`${country}`]["recovered"]
-	document.getElementById('death2').innerText = bData[`${country}`]["death"]
+function parseJSON1(country) {
+	document.getElementById('countryName2').innerText = bData[`${country}`]["countryName"] + getDateString();
+	document.getElementById('newCase2').innerText = bData[`${country}`]["newCase"];
+	document.getElementById('totalCase2').innerText = bData[`${country}`]["totalCase"];
+	document.getElementById('recovered2').innerText = bData[`${country}`]["recovered"];
+	document.getElementById('death2').innerText = bData[`${country}`]["death"];
 }
-function setRank1(bData) {
+function setRank1(data) {
 	const name = ["seoul","busan","daegu","incheon","gwangju","daejeon","ulsan","sejong",
 				"gyeonggi","gangwon","chungbuk","chungnam","jeonbuk","jeonnam","gyeongbuk",
 				"gyeongnam","jeju"]
 	let len = name.length; let top = ""; let max = 0; let index = 0;
 	let loc = []; let count = []; let rankName = []; let rankCount = [];
 	for(a of name) {
-		loc.push(`${bData[`${a}`][`countryName`]}`);
-		count.push(`${bData[`${a}`][`newCase`]}`);
+		loc.push(`${data[`${a}`][`countryName`]}`);
+		count.push(`${data[`${a}`][`newCase`]}`);
 	}
 	//1~5등 구하기
 	for (let k = 0; k < 5; k++) {
@@ -59,13 +62,29 @@ function setRank1(bData) {
 	}
 }
 //data2/////////////////////////////////////////////////////////////
-function parseJSON2(aData, jsonObj){
-	
+function parseJSON2(aData){
+	document.getElementById('countryName3').innerText = "전국" + getDateString();
+	document.getElementById('newCase3').innerText = aData[`korea`]["incDec"]
+	document.getElementById('totalCase3').innerText = aData[`korea`]["totalCnt"]
+	document.getElementById('recovered3').innerText = aData[`korea`]["recCnt"]
+	document.getElementById('death3').innerText = aData[`korea`]["deathCnt"]
 }
 function setRank2(aData) {
 	for(let i = 1; i < 6; i++) {
 		document.getElementById(`rankL${i}`).innerText = `${i}. ` + aData['API']['topCountries'][`country${i}N`];
 		document.getElementById(`rCountL${i}`).innerText = aData['API']['topCountries'][`country${i}P`] + "%";
 	}
+}
+//////////////////////////////////////////////////////////
+function getDateString() {
+	let today = new Date();
+	let year = today.getFullYear();
+	let month = today.getMonth();
+	let day = today.getDay();
+	year = year - parseInt(year / 100) * 100;
+	if (month < 10) month = "0" + month;
+	if (day < 10) day = "0" + day;
+
+	return `(${year}/${month}/${day})`;
 }
 getAPI();
--- 04m_BackEnd_1900_KDJ/DevRoot/Dropbox/Web/JavaWork/Lec_WEB/public/00_myweb/js/getAPI2.js
+++ 04m_BackEnd_1900_KDJ/DevRoot/Dropbox/Web/JavaWork/Lec_WEB/public/00_myweb/js/getAPI2.js
@@ -1,28 +1,75 @@
-//const API_KEY = "J8Trk79a5dDBsVwfEuSXyoNnLG4OHKmZp"
-let aData = [];
+const API_KEY = "J8Trk79a5dDBsVwfEuSXyoNnLG4OHKmZp";
+let cData = [];
 
-function loadAData() {
+function getAPI() {
    let url, xhttp;
-   url = `https://api.corona-19.kr/korea/beta/?serviceKey=${API_KEY}`;
-   xhttp = new XMLHttpRequest();
+   url = `https://api.corona-19.kr/korea/vaccine/?serviceKey=${API_KEY}`;
+	xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
-		if(this.readyState == 4 && this.status == 200){
-			aData = JSON.parse(this.responseText);
-         console.log(aData);
-			setRanka();
-			parseJSONa(JSON.parse(this.responseText));
+		if(this.readyState == 4 && this.status == 200) {
+			//데이터 받고 실행할 함수
+			let data = JSON.parse(this.responseText);
+         cData = data;
+         cData["korea"]["countryNm"] = "전국";
+			parseJSON(document.getElementById('countrybox').value);
 		}
 	};
 	xhttp.open("GET", url, true);
 	xhttp.send();
 }
-const parseJSONa = function(jsonObj){
-	
+function parseJSON(country) {
+	document.getElementById('countryName').innerText = cData[`${country}`]["countryNm"] + getDateString();
+   let pop = 517800;
+   const name = {"korea":517800, "seoul":97760,"busan":34290, "daegu":24650, 
+               "incheon":29230, "gwangju":15010, "daejeon":15310, "ulsan":11660, 
+               "sejong":2756, "gyeonggi":134100, "gangwon":15650, "chungbuk":15790, 
+               "chungnam":20600, "jeonbuk":18700, "jeonnam":19020, "gyeongbuk":27000, 
+               "gyeongnam":34480, "jeju":6342};
+   for(let i = 1; i < 5; i++) {
+      let text = cData[`${country}`][`vaccine_${i}`][`vaccine_${i}`];
+      let per = Math.round(parseInt(cData[`${country}`][`vaccine_${i}`][`vaccine_${i}`]) / parseInt(name[`${country}`]) * 100) / 100;
+	   if (per > 99) per = 99.2;
+      document.getElementById(`vaccine${i}`).innerText = `${text}명`;
+      document.getElementById(`percent${i}`).innerText = ` (${per}%)`;
+
+      document.getElementById(`bar${i}`).style.height = `${per}%`;
+   }
+
 }
-function setRanka() {
-	for(let i = 1; i < 6; i++) {
-		document.getElementById[`rankL${i}`] = aData['API']['topCountries'][`country${i}N`];
-		document.getElementById[`rCountL${i}`] = aData['API']['topCountries'][`country${i}P`];
+function getDateString() {
+	let today = new Date();
+	let year = today.getFullYear();
+	let month = today.getMonth();
+	let day = today.getDay();
+	year = year - parseInt(year / 100) * 100;
+	if (month < 10) month = "0" + month;
+	if (day < 10) day = "0" + day;
+
+	return `(${year}/${month}/${day})`;
+}
+/*
+function setRank1(data) {
+	const name = ["seoul","busan","daegu","incheon","gwangju","daejeon","ulsan","sejong",
+				"gyeonggi","gangwon","chungbuk","chungnam","jeonbuk","jeonnam","gyeongbuk",
+				"gyeongnam","jeju"]
+	let len = name.length; let top = ""; let max = 0; let index = 0;
+	let loc = []; let count = []; let rankName = []; let rankCount = [];
+	for(a of name) {
+		loc.push(`${data[`${a}`][`countryName`]}`);
+		count.push(`${data[`${a}`][`newCase`]}`);
+	}
+	//1~5등 구하기
+	for (let k = 0; k < 5; k++) {
+		for(let i = 0; i < len; i++) {if (max < parseInt(count[i])) {max = count[i]; top = loc[i]; index = i}}
+		rankName.push(top);
+		rankCount.push(max);
+		count[index] = 0;
+		max = 0; top = ""; index = 0;
+	}
+	for(let i = 0; i < 5; i++) {
+		document.getElementById(`rank${i + 1}`).innerText = `${i + 1}.  ${rankName[i]}`;
+		document.getElementById(`rCount${i + 1}`).innerText = `${rankCount[i]}▲`;
 	}
 }
-loadAData();
+*/
+getAPI();
