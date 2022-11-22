f34f8b27c0c2b4766cc5a6d2d253f6ddb2d73dd8
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-11-22 22:12:31 +0900

    BaekJoon

---
+++ 04m_BackEnd_1900_KDJ/DevRoot/Dropbox/Web/JavaWork/Backjoon/src/q16백트래킹03/Main.java
@@ -0,0 +1,47 @@
+package q16백트래킹03;
+/*
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static int[] res;
+	public static StringBuilder sb = new StringBuilder();
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+ 
+		res = new int[M];
+		func(N, M, 0);
+		System.out.println(sb);
+	}
+	public static void func(int N, int M, int depth) {
+		if (depth == M) {
+			for (int a : res) sb.append(a).append(" ");
+			sb.append('\n');
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			res[depth] = i;
+			func(N, M, depth + 1);
+		}
+	}
+}
