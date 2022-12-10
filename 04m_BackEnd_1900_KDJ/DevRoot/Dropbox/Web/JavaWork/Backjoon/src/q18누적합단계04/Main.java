584777a86b1646c439c930421f44ba73e4877bcb
kdjdj77 <51112376+kdjdj77@users.noreply.github.com>
2022-12-11 05:22:42 +0900

    update

---
+++ 04m_BackEnd_1900_KDJ/DevRoot/Dropbox/Web/JavaWork/Backjoon/src/q18누적합단계04/Main.java
@@ -0,0 +1,17 @@
+package q18누적합단계04;
+/*
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st;
+	}
+}
